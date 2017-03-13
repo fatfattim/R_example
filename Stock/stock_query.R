7150dc3e29e0702c788c762dc44bb082a769671c
TimChen <timchen@kkbox.com>
2017-03-13 22:57:22 +0800

    Query stock info of one day

--- Stock/stock_query.R
+++ Stock/stock_query.R
@@ -9,15 +9,30 @@ url <- "http://www.tse.com.tw/ch/trading/exchange/BWIBBU/BWIBBU_d.php"
 
 fd <- list(
   submit = "Show Prices",
-  input_date  = "106/03/10",
+  input_date  = "106/03/13",
   select2 = "ALL",
   order   = "STKNO",
   login_btn = "(unable to decode value)"
 )
 
 resp<-POST(url, body=fd, encode="form")
-haha <- content(resp,  encoding = "big-5")
+haha <- content(resp, encoding = "big-5")
 doc_string <- as.character(haha)
 doc <- htmlParse(doc_string,asText=T)
-id_or_class_xp <- "//select[@id='support-links']//text()"
-xpathSApply( doc,id_or_class_xp,xmlValue)
+id_or_class_xp <- "//div[@id='tbl-containerx']//text()"
+
+#These functions provide a way to find XML nodes that match a particular criterion.
+stockTableResult <- xpathSApply(doc, id_or_class_xp, xmlValue)
+#remove useless description
+stockTableResult <- stockTableResult[-1]
+#remove \n
+stockTableResult <- stockTableResult[ - which(stockTableResult %in% "\n")]
+x <- 1
+result <- c(stockTableResult[x:(x+4)])
+x <- 6
+while (x <= length(stockTableResult)) { 
+  result <- rbind(result, stockTableResult[x:(x+4)])
+  x <- x + 5
+}  
+
+head(result, n = 20)
